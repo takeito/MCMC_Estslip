42d3420cb05cd41b05b4cd72134553066cc6450d
木村洋 <kimurahiroshi@kimurahiroshi-no-MacBook-Pro.local>
2016-08-17 10:28:00 +0900

    "Fix the plate motion to slip vector(strike, dip tensile)."

--- make_interface_tri.m
+++ make_interface_tri.m
@@ -9,8 +9,8 @@ savefolder='./figs/Mesh';
 anim_savefile='./figs/tri_anim.gif';
 
 % ------------------Parameter---------------------
-PRM.NMESH=1000;
-Reducerate=0.001; % Reduce rate of triangles
+PRM.NMESH=100;
+Reducerate=0.01; % Reduce rate of triangles
 pole.lon=161.65; pole.lat=54.74; pole.omega=-1.168;   % PH plate motion relative to AM plate(REVEL)
 % --------------------------------------------------
 
@@ -97,23 +97,20 @@ alon0=(mean(OBS(1).ALON)+mean(S.lon))./2;
 tg.lon=mean(S.lon(S.tri),2);
 tg.lat=mean(S.lat(S.tri),2);
 tg.dep=mean(S.dep(S.tri),2);
-[sxyz]=enu2xyz(S.lon,S.lat,S.dep);
-% [tgxyz]=enu2xyz(tg.lon,tg.lat,tg.dep);
-[PM]=platemotion(tg.lon,tg.lat,POLE.lon,POLE.lat,POLE.omega);
+[tgxyz]=enu2xyz(tg.lon,tg.lat,6371D0);
+[pxyz]=enu2xyz(POLE.lon,POLE.lat,deg2rad(POLE.omega));
+[PMEN]=platemotion(tgxyz.x,tgxyz.y,tgxyz.z,tg.lon,tg.lat,pxyz.x,pxyz.y,pxyz.z);
 [gx,gy]=PLTXY(OBS(1).ALAT,OBS(1).ALON,alat0,alon0);
 [sx,sy]=PLTXY(S.lat,S.lon,alat0,alon0);
 gz=OBS(1).AHIG./1000;
 sz=S.dep;
 Ua=zeros(length(S.tri(:,1)),1);
-parfor n=1:length(S.tri)
-% for n=1:length(S.tri)
-    [SDT]=SDTvec(sxyz.X(S.tri(n,:)),sxyz.Y(S.tri(n,:)),sxyz.Z(S.tri(n,:)),tg.lon(n),tg.lat(n));
-    [pmxy]=xyz2XYplane(PM.X(n),PM.Y(n),PM.Z(n),tg.lon(n),tg.lat(n));
-    [pmXY]=[pmxy.X pmxy.Y 0];
-    pmcom=pmXY*SDT;
-    pmcom=pmcom./norm(pmcom);
-%     [U]=CalcTriDisps(gx',gy',gz',sx(S.tri(n,:)),sy(S.tri(n,:)),sz(S.tri(n,:)),0.25,0,0,1);
-    [U]=CalcTriDisps(gx',gy',gz',sx(S.tri(n,:)),sy(S.tri(n,:)),sz(S.tri(n,:)),0.25,pmcom(1),pmcom(3),pmcom(2));
+% parfor n=1:length(S.tri)
+for n=1:length(S.tri)        % <<--------- use in debug test
+    [SDT]=SDTvec(sx(S.tri(n,:)),sy(S.tri(n,:)),sz(S.tri(n,:)));
+    PMcom=[PMEN.EW(n) PMEN.NS(n) 0]*SDT;
+    PMcom=PMcom./norm(PMcom);
+    [U]=CalcTriDisps(gx',gy',gz',sx(S.tri(n,:)),sy(S.tri(n,:)),sz(S.tri(n,:)),0.25,PMcom(1),PMcom(3),PMcom(2));
     Ua(n)=sum(sqrt(U.x.^2+U.y.^2+U.z.^2));
 end
 Ntri=length(S.tri);
@@ -180,6 +177,7 @@ while Ntri > n_mesh
 %       Stri(nn,:)=S.tri(n,:);
 %     end  
 %   end
+
   glon=mean(S.lon(S.tri),2);
   glat=mean(S.lat(S.tri),2);
   ID=inpolygon(glon,glat,S.bound(:,1),S.bound(:,2));
@@ -188,32 +186,29 @@ while Ntri > n_mesh
   Stri=S.tri(ID1ind,:);
   tg.lon=mean(S.lon(Stri),2);
   tg.lat=mean(S.lat(Stri),2);
-  [sxyz]=enu2xyz(S.lon,S.lat,S.dep);
-  [PM]=platemotion(tg.lon,tg.lat,POLE.lon,POLE.lat,POLE.omega);
+  [tgxyz]=enu2xyz(tg.lon,tg.lat,6371D0);
+  [PMEN]=platemotion(tgxyz.x,tgxyz.y,tgxyz.z,tg.lon,tg.lat,pxyz.x,pxyz.y,pxyz.z);
+  
 %---------
   Ua_tmp=Ua;
   Ua=zeros(nn,1);   
-try
-    parfor n=1:nn
-        if Stri(n,1)~=S.tri(n,1) || Stri(n,2)~=S.tri(n,2) || Stri(n,3)~=S.tri(n,3)
-            [SDT]=SDTvec(sxyz.X(Stri(n,:)),sxyz.Y(Stri(n,:)),sxyz.Z(Stri(n,:)),tg.lon(n),tg.lat(n));
-            [pmxy]=xyz2XYplane(PM.X(n),PM.Y(n),PM.Z(n),tg.lon(n),tg.lat(n));
-            [pmXY]=[pmxy.X pmxy.Y 0];
-%             [SDT]=SDTvec(S.lon(S.tri(n,:)),S.lat(S.tri(n,:)),S.dep(S.tri(n,:)));
-%             [SDT]=SDTvec(S.lon(Stri(n,:)),S.lat(Stri(n,:)),S.dep(Stri(n,:)));
-%             pmcom=PM(n,:)*SDT;
-            pmcom=pmXY*SDT;
-            pmcom=pmcom./norm(pmcom);            
-            %      [U]=CalcTriDisps(gx',gy',gz',sx(Stri(n,:)),sy(Stri(n,:)),sz(Stri(n,:)),0.25,0,0,1);
-            [U]=CalcTriDisps(gx',gy',gz',sx(Stri(n,:)),sy(Stri(n,:)),sz(Stri(n,:)),0.25,pmcom(1),pmcom(3),pmcom(2));
-            Ua(n)=sum(sqrt(U.x.^2+U.y.^2+U.z.^2));
-        else
-            Ua(n)=Ua_tmp(n);
-        end
+% try
+%     for n=1:nn   %    <<----------------- use in debug test
+parfor n=1:nn
+    if Stri(n,1)~=S.tri(n,1) || Stri(n,2)~=S.tri(n,2) || Stri(n,3)~=S.tri(n,3)
+        [SDT]=SDTvec(sx(Stri(n,:)),sy(Stri(n,:)),sz(Stri(n,:)));
+        PMcom=[PMEN.EW(n) PMEN.NS(n) 0]*SDT;
+        PMcom=PMcom./norm(PMcom);
+        %      [U]=CalcTriDisps(gx',gy',gz',sx(Stri(n,:)),sy(Stri(n,:)),sz(Stri(n,:)),0.25,0,0,1);
+        [U]=CalcTriDisps(gx',gy',gz',sx(Stri(n,:)),sy(Stri(n,:)),sz(Stri(n,:)),0.25,PMcom(1),PMcom(3),PMcom(2));
+        Ua(n)=sum(sqrt(U.x.^2+U.y.^2+U.z.^2));
+    else
+        Ua(n)=Ua_tmp(n);
     end
-catch
-    keyboard
 end
+% catch
+%     keyboard
+% end
  
   S.tri=Stri;
   Ntri=length([S.tri]);
@@ -433,7 +428,6 @@ for iTri = 1:3
    % Calculate strike and dip of current leg
    strike                   = 180/pi*(atan2(y(iTri+1)-y(iTri), x(iTri+1)-x(iTri)));
    segMapLength             = sqrt((x(iTri)-x(iTri+1))^2 + (y(iTri)-y(iTri+1))^2);
-   [rx,~]                  = RotateXyVec(x(iTri+1)-x(iTri), y(iTri+1)-y(iTri), -strike);
    [rx,ry]                  = RotateXyVec(x(iTri+1)-x(iTri), y(iTri+1)-y(iTri), -strike);
    dip                      = 180/pi*(atan2(z(iTri+1)-z(iTri), rx));
 %   
@@ -544,8 +538,6 @@ R2bar             = y1.*y1 + y2.*y2 + y3bar.*y3bar;
 Rbar              = sqrt(R2bar);
 F                 = -atan2(y2, y1) + atan2(y2, z1) + atan2(y2.*R.*sinbeta, y1.*z1+(y2.*y2).*cosbeta);
 Fbar              = -atan2(y2, y1) + atan2(y2, z1bar) + atan2(y2.*Rbar.*sinbeta, y1.*z1bar+(y2.*y2).*cosbeta);
-denom1            = 8.*pi.*(1-nu);   %add by Kimura(test)
-denom2            = denom1./2;   %add by Kimura(test)
 
 % Case I: Burgers vector (B1,0,0)
 v1InfB1           = 2.*(1-nu).*(F+Fbar) - y1.*y2.*(1./(R.*(R-y3)) + 1./(Rbar.*(Rbar+y3bar))) - ...
@@ -553,13 +545,9 @@ v1InfB1           = 2.*(1-nu).*(F+Fbar) - y1.*y2.*(1./(R.*(R-y3)) + 1./(Rbar.*(R
 v2InfB1           = (1-2.*nu).*(log(R-y3)+log(Rbar+y3bar) - cosbeta.*(log(R-z3)+log(Rbar+z3bar))) - ...
                     y2.*y2.*(1./(R.*(R-y3))+1./(Rbar.*(Rbar+y3bar)) - cosbeta.*(1./(R.*(R-z3))+1./(Rbar.*(Rbar+z3bar))));
 v3InfB1           = y2 .* (1./R - 1./Rbar - cosbeta.*((R.*cosbeta-y3)./(R.*(R-z3)) - (Rbar.*cosbeta+y3bar)./(Rbar.*(Rbar+z3bar))));
-% v1InfB1           = v1InfB1 ./ (8.*pi.*(1-nu));
-% v2InfB1           = v2InfB1 ./ (8.*pi.*(1-nu));
-% v3InfB1           = v3InfB1 ./ (8.*pi.*(1-nu));
-v1InfB1           = v1InfB1 ./ denom1;
-v2InfB1           = v2InfB1 ./ denom1;
-v3InfB1           = v3InfB1 ./ denom1;
-
+v1InfB1           = v1InfB1 ./ (8.*pi.*(1-nu));
+v2InfB1           = v2InfB1 ./ (8.*pi.*(1-nu));
+v3InfB1           = v3InfB1 ./ (8.*pi.*(1-nu));
 
 v1CB1             = -2.*(1-nu).*(1-2.*nu).*Fbar.*(cotbeta.*cotbeta) + (1-2.*nu).*y2./(Rbar+y3bar) .* ((1-2.*nu-a./Rbar).*cotbeta - y1./(Rbar+y3bar).*(nu+a./Rbar)) + ...
                     (1-2.*nu).*y2.*cosbeta.*cotbeta./(Rbar+z3bar).*(cosbeta+a./Rbar) + a.*y2.*(y3bar-a).*cotbeta./(Rbar.*Rbar.*Rbar) + ...
@@ -575,13 +563,9 @@ v3CB1             = 2.*(1-nu).*(((1-2.*nu).*Fbar.*cotbeta) + (y2./(Rbar+y3bar).*
                     y2.*(y3bar-a)./Rbar.*(2.*nu./(Rbar+y3bar)+a./(Rbar.*Rbar)) + ...
                     y2.*(y3bar-a).*cosbeta./(Rbar.*(Rbar+z3bar)).*(1-2.*nu-(Rbar.*cosbeta+y3bar)./(Rbar+z3bar).*(cosbeta + a./Rbar) - a.*y3bar./(Rbar.*Rbar));
 
-% v1CB1             = v1CB1 ./ (4.*pi.*(1-nu));
-% v2CB1             = v2CB1 ./ (4.*pi.*(1-nu));
-% v3CB1             = v3CB1 ./ (4.*pi.*(1-nu));
-v1CB1             = v1CB1 ./ denom2;
-v2CB1             = v2CB1 ./ denom2;
-v3CB1             = v3CB1 ./ denom2;
-
+v1CB1             = v1CB1 ./ (4.*pi.*(1-nu));
+v2CB1             = v2CB1 ./ (4.*pi.*(1-nu));
+v3CB1             = v3CB1 ./ (4.*pi.*(1-nu));
 
 v1B1              = v1InfB1 + v1CB1;
 v2B1              = v2InfB1 + v2CB1;
@@ -593,12 +577,9 @@ v1InfB2           = -(1-2.*nu).*(log(R-y3) + log(Rbar+y3bar)-cosbeta.*(log(R-z3)
                     y1.*y1.*(1./(R.*(R-y3))+1./(Rbar.*(Rbar+y3bar))) + z1.*(R.*sinbeta-y1)./(R.*(R-z3)) + z1bar.*(Rbar.*sinbeta-y1)./(Rbar.*(Rbar+z3bar));
 v2InfB2           = 2.*(1-nu).*(F+Fbar) + y1.*y2.*(1./(R.*(R-y3))+1./(Rbar.*(Rbar+y3bar))) - y2.*(z1./(R.*(R-z3))+z1bar./(Rbar.*(Rbar+z3bar)));
 v3InfB2           = -(1-2.*nu).*sinbeta.*(log(R-z3)-log(Rbar+z3bar)) - y1.*(1./R-1./Rbar) + z1.*(R.*cosbeta-y3)./(R.*(R-z3)) - z1bar.*(Rbar.*cosbeta+y3bar)./(Rbar.*(Rbar+z3bar));
-% v1InfB2           = v1InfB2 ./ (8.*pi.*(1-nu));
-% v2InfB2           = v2InfB2 ./ (8.*pi.*(1-nu));
-% v3InfB2           = v3InfB2 ./ (8.*pi.*(1-nu));
-v1InfB2           = v1InfB2 ./ denom1;
-v2InfB2           = v2InfB2 ./ denom1;
-v3InfB2           = v3InfB2 ./ denom1;
+v1InfB2           = v1InfB2 ./ (8.*pi.*(1-nu));
+v2InfB2           = v2InfB2 ./ (8.*pi.*(1-nu));
+v3InfB2           = v3InfB2 ./ (8.*pi.*(1-nu));
 
 v1CB2             = (1-2.*nu).*((2.*(1-nu).*(cotbeta.*cotbeta)+nu).*log(Rbar+y3bar) - (2.*(1-nu).*(cotbeta.*cotbeta)+1).*cosbeta.*log(Rbar+z3bar)) + ...
                     (1-2.*nu)./(Rbar+y3bar).* (-(1-2.*nu).*y1.*cotbeta+nu.*y3bar-a+a.*y1.*cotbeta./Rbar + (y1.*y1)./(Rbar+y3bar).*(nu+a./Rbar)) - ...
@@ -613,12 +594,9 @@ v3CB2             = -2.*(1-nu).*(1-2.*nu).*cotbeta .* (log(Rbar+y3bar)-cosbeta.*
                     2.*(1-nu).*y1./(Rbar+y3bar).*(2.*nu+a./Rbar) + 2.*(1-nu).*z1bar./(Rbar+z3bar).*(cosbeta+a./Rbar) + ...
                    (y3bar-a)./Rbar.*((1-2.*nu).*cotbeta-2.*nu.*y1./(Rbar+y3bar)-a.*y1./(Rbar.*Rbar)) - ...
                    (y3bar-a)./(Rbar+z3bar).*(cosbeta.*sinbeta + (Rbar.*cosbeta+y3bar).*cotbeta./Rbar.*(2.*(1-nu).*cosbeta - (Rbar.*cosbeta+y3bar)./(Rbar+z3bar)) + a./Rbar.*(sinbeta - y3bar.*z1bar./(Rbar.*Rbar) - z1bar.*(Rbar.*cosbeta+y3bar)./(Rbar.*(Rbar+z3bar))));
-% v1CB2             = v1CB2 ./ (4.*pi.*(1-nu));
-% v2CB2             = v2CB2 ./ (4.*pi.*(1-nu));
-% v3CB2             = v3CB2 ./ (4.*pi.*(1-nu));
-v1CB2             = v1CB2 ./ denom2;
-v2CB2             = v2CB2 ./ denom2;
-v3CB2             = v3CB2 ./ denom2;
+v1CB2             = v1CB2 ./ (4.*pi.*(1-nu));
+v2CB2             = v2CB2 ./ (4.*pi.*(1-nu));
+v3CB2             = v3CB2 ./ (4.*pi.*(1-nu));
 
 v1B2              = v1InfB2 + v1CB2;
 v2B2              = v2InfB2 + v2CB2;
@@ -629,13 +607,9 @@ v3B2              = v3InfB2 + v3CB2;
 v1InfB3           = y2.*sinbeta.*((R.*sinbeta-y1)./(R.*(R-z3))+(Rbar.*sinbeta-y1)./(Rbar.*(Rbar+z3bar)));
 v2InfB3           = (1-2.*nu).*sinbeta.*(log(R-z3)+log(Rbar+z3bar)) - (y2.*y2).*sinbeta.*(1./(R.*(R-z3))+1./(Rbar.*(Rbar+z3bar)));
 v3InfB3           = 2.*(1-nu).*(F-Fbar) + y2.*sinbeta.*((R.*cosbeta-y3)./(R.*(R-z3))-(Rbar.*cosbeta+y3bar)./(Rbar.*(Rbar+z3bar)));
-% v1InfB3           = v1InfB3 ./ (8.*pi.*(1-nu));
-% v2InfB3           = v2InfB3 ./ (8.*pi.*(1-nu));
-% v3InfB3           = v3InfB3 ./ (8.*pi.*(1-nu));
-v1InfB3           = v1InfB3 ./ denom1;
-v2InfB3           = v2InfB3 ./ denom1;
-v3InfB3           = v3InfB3 ./ denom1;
-
+v1InfB3           = v1InfB3 ./ (8.*pi.*(1-nu));
+v2InfB3           = v2InfB3 ./ (8.*pi.*(1-nu));
+v3InfB3           = v3InfB3 ./ (8.*pi.*(1-nu));
 
 v1CB3             = (1-2.*nu).*(y2./(Rbar+y3bar).*(1+a./Rbar) - y2.*cosbeta./(Rbar+z3bar).*(cosbeta+a./Rbar)) - ...
                     y2.*(y3bar-a)./Rbar.*(a./(Rbar.*Rbar) + 1./(Rbar+y3bar)) + ...
@@ -646,13 +620,9 @@ v2CB3             = (1-2.*nu).*(-sinbeta.*log(Rbar+z3bar) - y1./(Rbar+y3bar).*(1
                     1./(Rbar.*(Rbar+z3bar)).*((y2.*y2).*cosbeta.*sinbeta - a.*z1bar./Rbar.*(Rbar.*cosbeta+y3bar)));
 v3CB3             = 2.*(1-nu).*Fbar + 2.*(1-nu).*(y2.*sinbeta./(Rbar+z3bar).*(cosbeta + a./Rbar)) + ...
                     y2.*(y3bar-a).*sinbeta./(Rbar.*(Rbar+z3bar)).*(1 + (Rbar.*cosbeta+y3bar)./(Rbar+z3bar).*(cosbeta+a./Rbar) + a.*y3bar./(Rbar.*Rbar));
-% v1CB3             = v1CB3 ./ (4.*pi.*(1-nu));
-% v2CB3             = v2CB3 ./ (4.*pi.*(1-nu));
-% v3CB3             = v3CB3 ./ (4.*pi.*(1-nu));
-v1CB3             = v1CB3 ./ denom2;
-v2CB3             = v2CB3 ./ denom2;
-v3CB3             = v3CB3 ./ denom2;
-
+v1CB3             = v1CB3 ./ (4.*pi.*(1-nu));
+v2CB3             = v2CB3 ./ (4.*pi.*(1-nu));
+v3CB3             = v3CB3 ./ (4.*pi.*(1-nu));
 
 v1B3              = v1InfB3 + v1CB3;
 v2B3              = v2InfB3 + v2CB3;
@@ -665,71 +635,51 @@ v2                = B1.*v2B1 + B2.*v2B2 + B3.*v2B3;
 v3                = B1.*v3B1 + B2.*v3B2 + B3.*v3B3;
 end
 %====================================================
-function [PM] = platemotion(triglon,triglat,plon,plat,omega)
+function [VEN] = platemotion(trigx,trigy,trigz,tlon,tlat,px,py,pz)
 
 % Input-------------------------------------------------
-%   plon, plat            :Pole position
-%   omega                 :Angular velocity
-%   triglon, triglat      :center of triangles
+%   trigx,trigy,trigz     :Center of triangle position
+%   tlon,tlat             :Longitude and Latitude of center of triangle
+%   px,py,pz              :Pole
 % Output------------------------------------------------
-%   PM.X, PM.Y, PM.Z      :plate motion(XYZ-coordinate)[km/yr]
+%   V.EW, V.NS            :plate motion[km/yr] (Positive directions are E and N)
 % ------------------------------------------------------
 
-% Parameter
-amean=6371D3;                 % radius of the Earth
-
-% Transformation from degree to radian
-plon=deg2rad(plon);
-plat=deg2rad(plat);
-omega=deg2rad(omega);
-triglon=deg2rad(triglon);
-triglat=deg2rad(triglat);
+tlon=deg2rad(tlon);
+tlat=deg2rad(tlat);
 
 % Transformation from pole coord. to XYZ
-[polexyz]=([cos(plat).*cos(plon) cos(plat).*sin(plon) sin(plat)].*omega)';
-[trigxyz]=([cos(triglat).*cos(triglon) cos(triglat).*sin(triglon) sin(triglat)].*amean)';
+[polexyz]=[px; py; pz];
+[trigxyz]=[trigx'; trigy'; trigz'];
 
-% Plate motion
+% Calculate plate motion
 [~,trigsize]=size(trigxyz);
 pm=zeros(3,trigsize);
 for ii=1:trigsize
     pm(:,ii)=cross(polexyz,trigxyz(:,ii)).*1.0D-6;
 end
 % PM=PM';
-PM.X=pm(1,:);
-PM.Y=pm(2,:);
-PM.Z=pm(3,:);
+Vx=pm(1,:);
+Vy=pm(2,:);
+Vz=pm(3,:);
 
+% Export to E-N direction Velocity
+VEN.EW=           -sin(tlon).*Vx' +            cos(tlon).*Vy';
+VEN.NS=-sin(tlat).*cos(tlon).*Vx' - sin(tlat).*sin(tlon).*Vy' + cos(tlat).*Vz';
+VEN.UD= cos(tlat).*cos(tlon).*Vx' + cos(tlat).*sin(tlon).*Vy' + sin(tlat).*Vz';    % not need
 
 end
 %====================================================
-function [SDT] = SDTvec(tvx,tvy,tvz,clon,clat)
+function [SDT] = SDTvec(tvx,tvy,tvz)
 
 % Calculate Strike, Dip, Tensile vectors of triangle
 % Input-----------------------------   
-%  tvx  : x-coordinate of triangle vertices.
-%  tvy  : y-coordinate of triangle vertices.
-%  tvz  : z-coordinate of triangle vertices.
+%  tvx  : x-coordinate(X) of triangle vertices. 
+%  tvy  : y-coordinate(Y) of triangle vertices. 
+%  tvz  : z-coordinate(dep) of triangle vertices. 
 % Output---------------------------
 %  SDT  : xyz-coordinates(XY-plane) of strike, dip, tensile vectors of triangle.
 % ---------------------------------
-
-% parameter
-amean=6371D3;      % equater of the Earth [m]
-
-% degree to radian
-clon=deg2rad(clon);
-clat=deg2rad(clat);
-
-% Rotation matrix
-Rot1=[cos(clon+pi./2) sin(clon+pi./2) 0; -sin(clon+pi./2) cos(clon+pi./2) 0; 0 0 1];
-Rot2=[1 0 0; 0 cos(pi./2-clat) sin(pi./2-clat); 0 -sin(pi./2-clat) cos(pi./2-clat)];
-
-% Transformation coordinate (xyz to XYplane)
-tXY=Rot2*Rot1*[tvx';tvy';tvz'];
-tvx=tXY(1,:);
-tvy=tXY(2,:);
-tvz=tXY(3,:);tvz=tvz-amean;     % set 0m at surface
 
 % Strike, Dip, Tensile vectors on xyz-coordinate
 normVec        = cross([tvx(2);tvy(2);tvz(2)]-[tvx(1);tvy(1);tvz(1)], [tvx(3);tvy(3);tvz(3)]-[tvx(1);tvy(1);tvz(1)]);
@@ -743,65 +693,25 @@ end
 strikeVec      = [-sin(atan2(normVec(2),normVec(1))) cos(atan2(normVec(2),normVec(1))) 0];
 dipVec         = cross(normVec, strikeVec);
 
-SDT=[strikeVec(:) dipVec(:) normVec(:)];
+SDT=[strikeVec(:) dipVec(:) zeros(3,1)];
 
 end
 %====================================================
-function [XYZ]=enu2xyz(tvlon,tvlat,tvdep)
-
-% parameter
-amean=6371D3;      % equater of the Earth [m]
+function [xyz]=enu2xyz(tvlon,tvlat,abs)
 
 % degree to radian
 tvlon=deg2rad(tvlon);
 tvlat=deg2rad(tvlat);
-tvdep=tvdep+amean;
 
 % lonlat to xyz
-tvx=cos(tvlat).*cos(tvlon).*tvdep;
-tvy=cos(tvlat).*sin(tvlon).*tvdep;
-tvz=sin(tvlat).*tvdep;
+tvx=cos(tvlat).*cos(tvlon).*abs;
+tvy=cos(tvlat).*sin(tvlon).*abs;
+tvz=sin(tvlat).*abs;
 
-XYZ.X=tvx;
-XYZ.Y=tvy;
-XYZ.Z=tvz;
+xyz.x=tvx;
+xyz.y=tvy;
+xyz.z=tvz;
 % XYZ=[tvx(:) tvy(:) tvz(:)];
-
-end
-%====================================================
-function [XYplane]=xyz2XYplane(x,y,z,clon,clat)
-
-% Transformation coordinate from xyz-coord. to XYplane-coord. which center is clon,clat
-% Input-----------------------------   
-%  x    : x-coordinate of triangle vertices.
-%  y    : y-coordinate of triangle vertices.
-%  z    : z-coordinate of triangle vertices.
-%  clon : center longitude of triangle.
-%  clat : center latgitude of triangle.
-% Output---------------------------
-%  XYplane  : xyz-coordinates(XY-plane) of strike, dip, tensile vectors of triangle.
-% ---------------------------------
-
-% parameter
-amean=6371D3;      % equater of the Earth [m]
-
-% degree to radian
-clon=deg2rad(clon);
-clat=deg2rad(clat);
-
-% Rotation matrix
-Rot1=[cos(clon+pi./2) sin(clon+pi./2) 0; -sin(clon+pi./2) cos(clon+pi./2) 0; 0 0 1];
-Rot2=[1 0 0; 0 cos(pi./2-clat) sin(pi./2-clat); 0 -sin(pi./2-clat) cos(pi./2-clat)];
-
-% Transformation coordinate (xyz to XYplane)
-tXY=Rot2*Rot1*[x;y;z];
-tvx=tXY(1,:);
-tvy=tXY(2,:);
-tvz=tXY(3,:);tvz=tvz-amean;     % set 0m at surface
-
-XYplane.X=tvx;
-XYplane.Y=tvy;
-XYplane.Z=tvz;
 
 end
 %====================================================
