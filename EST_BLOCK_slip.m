24d2041ecf72e7ff7e82c376896b4bd7c51b4d58
kimura <kimurahiroshi@hiroshi-mint>
2017-04-21 16:46:53 +0900

    "BUG FIX"

--- EST_BLOCK_slip.m
+++ EST_BLOCK_slip.m
@@ -540,7 +540,7 @@ for NB=1:BLK(1).NBlock
 end
 quiver(OBS(1).ALON,OBS(1).ALAT,OBS(1).EVEC,OBS(1).NVEC,'green')
 for NPL=1:PRM.NPL
-  quiver(OBS(1).BLON,OBS(1).BLAT,CHA.SMP(1:3:end,NPL)',CHA.SMP(2:3:end,NPL)','blue')
+  quiver(OBS(1).ALON,OBS(1).ALAT,CHA.SMP(1:3:end,NPL)',CHA.SMP(2:3:end,NPL)','blue')
   hold on
 end
 %
@@ -548,7 +548,7 @@ figure(106);clf(106)
 for NPL=1:PRM.NPL
   quiver(OBS(1).ALON,OBS(1).ALAT,OBS(1).EVEC,OBS(1).NVEC,'green')
   hold on
-  quiver(OBS(1).BLON,OBS(1).BLAT,CHA.SMP(1:3:end,NPL)',CHA.SMP(2:3:end,NPL)','blue')
+  quiver(OBS(1).ALON,OBS(1).ALAT,CHA.SMP(1:3:end,NPL)',CHA.SMP(2:3:end,NPL)','blue')
   hold on
   axis([OBS(1).LONMIN-1,OBS(1).LONMAX+1,OBS(1).LATMIN-1,OBS(1).LATMAX+1]);
   title(['Iteration Number: ',num2str(RT)]);
@@ -879,8 +879,6 @@ for NB1=1:BLK(1).NBlock
   end
 end
 %
-OBS(1).BLAT=[];
-OBS(1).BLON=[];
 for N=1:BLK(1).NBlock
   IND=inpolygon(OBS(1).ALON,OBS(1).ALAT,BLK(N).LON,BLK(N).LAT);
   OBS(1).ABLK(IND)=N;
@@ -897,8 +895,6 @@ for N=1:BLK(1).NBlock
   OBS(N).OXYZ=conv2ell(OBS(N).LAT,OBS(N).LON);
   OBS(N).Vne=reshape([OBS(1).EVEC(IND); OBS(1).NVEC(IND)],OBS(N).NBLK.*2,1);
   OBS(N).Vww=reshape([OBS(1).EERR(IND); OBS(1).NERR(IND)],OBS(N).NBLK.*2,1);
-  OBS(1).BLAT=[OBS(1).BLAT,OBS(N).LAT];
-  OBS(1).BLON=[OBS(1).BLON,OBS(N).LON];
 end
 end
 %% READ OBSERVATION DATA
