5439b8e94f29d322d4e58d170f88b7518e2106fb
hiroshi <hiroshi@hiroshi-dell>
2017-07-19 16:04:04 +0900

    "REVISE: Bin interval of McHISTOGRAM"

--- EXPAND_DATA.m
+++ EXPAND_DATA.m
@@ -17,7 +17,7 @@ NDATAFLT=zeros(NFLT,1);
 % 
 binnum=[-127.5:1:127];
 Mpbin=[-10^10:10^8:10^10];
-Mcbin=[-1:0.02:1];
+Mcbin=[-1:0.01:1];
 MpHIST=zeros(NPOL,size(Mpbin,2)-1);
 McHIST=zeros(NFLT,size(Mcbin,2)-1);
 
