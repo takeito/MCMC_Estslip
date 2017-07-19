fb757f128e528d7e7e613df6264fa1167ae055db
hiroshi <hiroshi@hiroshi-dell>
2017-07-19 17:31:55 +0900

    "REVISE: (for debug) save number of data."

--- EXPAND_DATA.m
+++ EXPAND_DATA.m
@@ -1,5 +1,6 @@
 function EXPAND_DATA(dir)
 % 
+[PRM]=READ_PARAMETERS(INPUT);
 file=[dir,'/CHA_test.mat'];
 load(file)
 % 
@@ -83,9 +84,62 @@ TCHA.CORPOL=CORPOL;
 TCHA.CORFLT=CORFLT;
 TCHA.HISTPOL=MpHIST;
 TCHA.HISTFLT=McHIST;
+TCHA.NDATPOL=NDATAPOL;
+TCHA.NDATFLT=NDATAFLT;
 % 
 outfile=[dir,'/TCHA.mat'];
 save(outfile,'TCHA');
 % save('./Result/Test_06/TCHA_test.mat','TCHA') % test
 
+end
+%% READ PARAMETER FOR MCMC Inversion 
+function [PRM]=READ_PARAMETERS(INPUT)
+% MCMC Inversion for Geodetic 
+% Coded    by Takeo Ito 2011/11/08 (ver 1.0)
+% Modified by Takeo Ito 2012/10/26 (ver 1.1)
+% Modified by Takeo Ito 2015/11/11 (ver 1.2)
+% Modified by Takeo Ito 2016/07/06 (ver 1.3)
+%
+Fid=fopen(INPUT.Parfile,'r');
+PRM.HOME_D=pwd;
+FileOBS=fscanf(Fid,'%s \n',[1,1]);
+PRM.FileOBS=fullfile(PRM.HOME_D,FileOBS);
+[~]=fgetl(Fid);
+DIRBlock=fscanf(Fid,'%s \n',[1,1]);
+PRM.DIRBlock=fullfile(PRM.HOME_D,DIRBlock);
+[~]=fgetl(Fid);
+DIRBlock_Interface=fscanf(Fid,'%s \n',[1,1]);
+PRM.DIRBlock_Interface=fullfile(PRM.HOME_D,DIRBlock_Interface);
+[~]=fgetl(Fid);
+%
+PRM.GPU=fscanf(Fid,'%d \n',[1,1]);
+[~]=fgetl(Fid);
+PRM.ITR=fscanf(Fid,'%d \n',[1,1]);
+[~]=fgetl(Fid);
+PRM.CHA=fscanf(Fid,'%d \n',[1,1]);
+[~]=fgetl(Fid);
+PRM.KEP=fscanf(Fid,'%d \n',[1,1]);
+[~]=fgetl(Fid);
+PRM.RWD=fscanf(Fid,'%f \n',[1,1]);
+fclose(Fid);
+%====================================================
+tmp=load(INPUT.Optfile);
+PRM.num=size(tmp,1);
+PRM.OptB1=tmp(:,1);
+PRM.OptB2=tmp(:,2);
+PRM.OptINT=tmp(:,3);
+%====================================================
+fprintf('==================\nINPUT PARAMETERS\n==================\n') 
+fprintf('HOME_D             : %s \n',PRM.HOME_D) 
+fprintf('FileOBS            : %s \n',PRM.FileOBS) 
+fprintf('DIRBlock           : %s \n',PRM.DIRBlock)
+fprintf('DIRBlock_Interface : %s \n',PRM.DIRBlock_Interface) 
+fprintf('GPUdev (CPU:99)    : %i \n',PRM.GPU) 
+fprintf('ITR(Max_Nitr)      : %i \n',PRM.ITR) 
+fprintf('CHA(Chain)         : %i \n',PRM.CHA) 
+fprintf('KEP(KEEP)          : %i \n',PRM.KEP) 
+fprintf('RWD(Walk_dis)      : %4.2f \n',PRM.RWD) 
+fprintf('==================\n') 
+%====================================================
+disp('PASS READ_PARAMETERS')
 end
