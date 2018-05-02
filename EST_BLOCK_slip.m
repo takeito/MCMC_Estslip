8c412d0edfb51c05e7a191940a08667ec1a14808
hiroshi <hiroshi@hiroshi-dell>
2018-05-02 11:58:34 +0900

    "REVISE add CHA.Mi compress process into COMPRESS_DATA"

--- EST_BLOCK_slip.m
+++ EST_BLOCK_slip.m
@@ -705,34 +705,43 @@ fclose(logFID);
 end
 %% Compress CHA sampling
 function COMPRESS_DATA(CHA,PRM,ITR,NACC)
-% 
-% load('./Result_red/Test_07/CHA.mat'); % test
+% Compressing CHA sampled parameter to int8
 % 
 CHA.Mc=single(CHA.Mc);
 CHA.Mp=single(CHA.Mp);
+CHA.Mi=single(CHA.Mi);
 % if PRM.GPU==99&&gpuDeviceCount==0
 if PRM.GPU==99
   MEANMc=mean(CHA.Mc,2);
   MEANMp=mean(CHA.Mp,2);
+  MEANMi=mean(CHA.Mi,2);
   COVMc=cov(CHA.Mc');
   COVMp=cov(CHA.Mp');
+  COVMi=cov(CHA.Mi');
 else
   gCHA.Mc=gpuArray(CHA.Mc);
   gCHA.Mp=gpuArray(CHA.Mp);
+  gCHA.Mi=gpuArray(CHA.Mi);
   MEANMc=mean(gCHA.Mc,2);
   MEANMp=mean(gCHA.Mp,2);
+  MEANMi=mean(gCHA.Mi,2);
   COVMc=cov(gCHA.Mc');
   COVMp=cov(gCHA.Mp');
+  COVMi=cov(gCHA.Mi');
   MEANMc=gather(MEANMc);
   MEANMp=gather(MEANMp);
+  MEANMi=gather(MEANMi);
   COVMc=gather(COVMc);
   COVMp=gather(COVMp);
+  COVMi=gather(COVMi);
 end
 % 
 McMAX=max(CHA.Mc,[],2);
 McMIN=min(CHA.Mc,[],2);
 MpMAX=max(CHA.Mp,[],2);
 MpMIN=min(CHA.Mp,[],2);
+MiMAX=max(CHA.Mi,[],2);
+MiMIN=min(CHA.Mi,[],2);
 % 
 Mcscale=100./(McMAX-McMIN);
 McBASE=bsxfun(@times,bsxfun(@minus,CHA.Mc,McMIN),Mcscale.*2.55-128);
@@ -740,6 +749,9 @@ Mcint8=int8(McBASE);
 Mpscale=100./(MpMAX-MpMIN);
 MpBASE=bsxfun(@times,bsxfun(@minus,CHA.Mp,MpMIN),Mpscale.*2.55-128);
 Mpint8=int8(MpBASE);
+Miscale=100./(MiMAX-MiMIN);
+MiBASE=bsxfun(@times,bsxfun(@minus,CHA.Mi,MiMIN),Miscale.*2.55-128);
+Miint8=int8(MiBASE);
 % 
 binedge=int8(-128:127);
 % 
@@ -762,6 +774,16 @@ end
 cha.MpCOMPRESS.COVMp=COVMp;
 cha.MpCOMPRESS.MEANMp=MEANMp;
 cha.MpCOMPRESS.SMPMp=int8(MpBASE);
+% 
+for ii=1:size(Miint8,1)
+  cha.MiCOMPRESS.NPOL(ii).Miscale=Miscale(ii);
+  cha.MiCOMPRESS.NPOL(ii).MiMAX=MiMAX(ii);
+  cha.MiCOMPRESS.NPOL(ii).MiMIN=MiMIN(ii);
+  cha.MiCOMPRESS.NPOL(ii).MiHIST=histcounts(Miint8(ii,:),binedge);
+end
+cha.MiCOMPRESS.COVMi=COVMi;
+cha.MiCOMPRESS.MEANMi=MEANMi;
+cha.MiCOMPRESS.SMPMi=int8(MiBASE);
 % 
 cha.AJR=NACC./PRM.CHA;
 save(fullfile(PRM.DirResult,['CHA_test',num2str(ITR,'%03i')]),'cha','-v7.3');
